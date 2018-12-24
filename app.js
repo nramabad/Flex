6121e48f511e44e95fb77b5d3a0c4cbbdfbae47c
nramabad <nramabad@gmail.com>
2018-12-24 11:59:45 -0800

    Set up initial file structure and entry file

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const db = require("./config/keys").mongoURI;
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
