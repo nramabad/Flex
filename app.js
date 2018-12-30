69d420f7291f2b145b6ef06f9fb6dc095d5532dd
nramabad <nramabad@gmail.com>
2018-12-30 00:24:59 -0800

    Fix app.js for config passport

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
-const users = require("./routes/api/users");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 mongoose
   .connect(
@@ -13,25 +15,16 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.get("/", (req, res) => res.send("Hello World!!"));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-app.get("/", (req, res) => {
-    const user = new User({
-        handle: 'jim',
-        email: 'jim@jim.jim',
-        password: 'jimothy'
-    })
-    user.save();
-    res.send("Hello World!");
-});
-
-
 
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
