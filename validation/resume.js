db20d6dbfb7e0659f047c079edf78f289c4b862d
nramabad <nramabad@gmail.com>
2019-01-03 08:47:14 -0800

    Add additional validation

--- validation/resume.js
+++ validation/resume.js
@@ -8,6 +8,10 @@ module.exports = function validateTweetInput(data) {
 
     data.text = validText(data.text) ? data.text : '';
 
+    if (!Validator.isLength(data.text, { min: 5, max: 38380 })) {
+        errors.text = 'Tweet must be between 5 and 140 characters';
+    }
+
     if (Validator.isEmpty(data.text)) {
         errors.text = 'Text field is required';
     }
