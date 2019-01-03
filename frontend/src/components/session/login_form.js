df00a9160ea21e357de4922aa2326a13b276857f
Adi Kantawala <adikan@ucla.edu>
2019-01-03 09:26:49 -0800

    trying to merge

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -20,7 +20,7 @@ class LoginForm extends React.Component {
     // Once the user has been authenticated, redirect to the Tweets page
     componentWillReceiveProps(nextProps) {
         if (nextProps.currentUser === true) {
-            this.props.history.push('/tweets');
+            this.props.history.push('/home');
         }
 
         // Set or clear errors
