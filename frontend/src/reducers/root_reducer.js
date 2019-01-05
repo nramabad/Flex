0fd3002cb91287e27a76173c794e0d2d401fd9af
gwang <guanw88@gmail.com>
2019-01-04 16:43:27 -0800

    Move Github api query to backend route

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,12 +3,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from "./errors_reducer";
-import jobs from "./jobs_reducer";
 
 const RootReducer = combineReducers({
     errors,
-    session,
-    jobs
+    session
 });
 
 export default RootReducer;
