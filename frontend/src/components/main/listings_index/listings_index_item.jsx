05dc71429db9b22bb5155c978832ba8f345d32dc
gwang <guanw88@gmail.com>
2019-01-06 13:19:06 -0800

    Pull job listings from database and style in left sidebar

--- frontend/src/components/main/listings_index/listings_index_item.jsx
+++ frontend/src/components/main/listings_index/listings_index_item.jsx
@@ -7,9 +7,9 @@ class ListingsIndexItem extends React.Component {
     return (
       <div className="listings-index-item-container">
         <div className="listings-index-item-left">
-          <div className="index-item-job-title">Job Title</div>
-          <div className="index-item-company">Great Company</div>
-          <div className="index-item-location">San Francisco Bay Area, CA</div>
+          <div className="index-item-job-title">{this.props.title}</div>
+          <div className="index-item-company">{this.props.company}</div>
+          <div className="index-item-location">{this.props.location}</div>
         </div>
         <div className="listings-index-item-right">
           <div className="index-item-match-percentage">87%</div>
