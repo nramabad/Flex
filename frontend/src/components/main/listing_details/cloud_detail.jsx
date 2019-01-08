e1289a92ee437283611fdd38cb22231b9398545a
nramabad <nramabad@gmail.com>
2019-01-07 17:55:52 -0800

    Fix bug in job route

--- frontend/src/components/main/listing_details/cloud_detail.jsx
+++ frontend/src/components/main/listing_details/cloud_detail.jsx
@@ -5,12 +5,9 @@ class CloudDetail extends React.Component {
 
     render() {
         const data = this.props.words.map(word => ({ text: word, value: 100 }) );
-
-        // this.props.words.map( word => { text: word, value: 100 } );
-
         const fontSizeMapper = word => Math.log2(word.value) * 5;
         const rotate = word => word.value % 360;
-        // debugger
+
         return (<WordCloud
             data={data}
             fontSizeMapper={fontSizeMapper}
